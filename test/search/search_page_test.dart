e3e8a7866c979d4be2458099d786bd48ef44bfdb
Robert Gray <gray419@gmail.com>
2021-04-24 00:56:17 -0400

    add more search page tests

--- test/search/search_page_test.dart
+++ test/search/search_page_test.dart
@@ -1,4 +1,5 @@
 import 'package:blizzard/search/view/search_page.dart';
+import 'package:blizzard/search/widgets/search_results.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,10 +10,14 @@ class MockWeatherRepository extends Mock implements WeatherRepository {}
 
 void main() {
   group('SearchPage', () {
+    final term = 'nyc';
+    final location = Location(1, ' New York');
     late WeatherRepository weatherRepository;
 
     setUp(() {
       weatherRepository = MockWeatherRepository();
+      when(() => weatherRepository.searchForLocation(term: term))
+          .thenAnswer((_) => Future.value([location]));
     });
 
     testWidgets('renders SearchPage', (tester) async {
@@ -23,7 +28,23 @@ void main() {
         ),
       ));
       await tester.pumpAndSettle();
+
       expect(find.byType(SearchForm), findsOneWidget);
+    });
+
+    testWidgets('renders SearchResults', (tester) async {
+      await tester.pumpWidget(RepositoryProvider.value(
+        value: weatherRepository,
+        child: MaterialApp(
+          home: SearchPage(),
+        ),
+      ));
+      await tester.pumpAndSettle();
+      expect(find.byType(SearchForm), findsOneWidget);
+      await tester.enterText(find.byKey(Key('searchBar_textField')), 'nyc');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+      expect(find.byType(SearchResults), findsOneWidget);
     });
   });
 }
