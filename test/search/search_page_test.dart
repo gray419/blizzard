7821af316562920d3e9801ec2f14189527399d72
Robert Gray <gray419@gmail.com>
2021-04-24 12:06:20 -0400

    add test for error

--- test/search/search_page_test.dart
+++ test/search/search_page_test.dart
@@ -46,5 +46,28 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.byType(SearchResults), findsOneWidget);
     });
+
+    group('errors', () {
+      final term = 'nyc';
+      setUp(() {
+        weatherRepository = MockWeatherRepository();
+        when(() => weatherRepository.searchForLocation(term: term))
+            .thenThrow((_) => Exception('error'));
+      });
+
+      testWidgets('renders SearchPage error view', (tester) async {
+        await tester.pumpWidget(RepositoryProvider.value(
+          value: weatherRepository,
+          child: MaterialApp(
+            home: SearchPage(),
+          ),
+        ));
+        await tester.pumpAndSettle();
+        await tester.enterText(find.byKey(Key('searchBar_textField')), 'nyc');
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pumpAndSettle();
+        expect(find.text('Error searching for that location'), findsOneWidget);
+      });
+    });
   });
 }
