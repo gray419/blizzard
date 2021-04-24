2811d74ff971b4d9de7b7755c8a32c4dc05faa90
Robert Gray <gray419@gmail.com>
2021-04-23 20:16:46 -0400

    add flutter bloc and provide weather repository

--- lib/blizzard.dart
+++ lib/blizzard.dart
@@ -1,13 +1,23 @@
 import 'package:blizzard/search/view/search_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 class BlizzardApp extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  const BlizzardApp({Key? key, required this.weatherRepository})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SearchPage(),
+    return RepositoryProvider.value(
+      value: weatherRepository,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SearchPage(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:blizzard/blizzard.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 void main() {
-  runApp(BlizzardApp());
+  runApp(BlizzardApp(
+    weatherRepository: WeatherRepository(),
+  ));
 }
